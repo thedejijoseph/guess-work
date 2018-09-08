5c0df380b75b1403e2726afab960ccefe67ab22e
Deji <wrecodde@gmail.com>
2018-09-08 16:51:19 -0700

    basic.py setup

--- basic.py
+++ basic.py
@@ -8,9 +8,8 @@
 # algo: y = (2 * x) - 1
 # model.predict(3) = 8
 # model.determine(8) = 3
-
 func_y = lambda x: (2 * x) - 1
-x
+
 X = range(1200)
 y = [func_y(i) for i in X]
 
@@ -20,14 +19,19 @@ import numpy as np
 from sklearn import utils
 
 dataset = utils.Bunch()
-dataset.data = np.array(X)
-dataset.target = np.array(y)
+dataset.data = np.array(X).reshape(-1, 1)
+dataset.target = np.array(y).reshape(-1, 1)
 
-from skelarn import linear_model
+from sklearn import linear_model
 model = linear_model.LinearRegression()
 
-model.fit(dataset.data, dataset.predict)
+model.fit(dataset.data, dataset.target)
 
-test_X = np.array(range(3, 1200, 2))
-test_y = np.array([func_y(i) for i in test_X])
+test_X = np.array([i for i in range(3, 1200)]).reshape(-1, 1)
+test_y = np.array([func_y(i) for i in test_X]).reshape(-1, 1)
+pred_y = model.predict(test_X)
 
+from sklearn import metrics
+scorer = metrics.explained_variance_score
+scorer(test_y, pred_y)
+# outputs 1.0
